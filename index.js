3982ac17916d0321d4c99b49e3e73fe77b932478
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2022-05-09 15:37:21 +1000

    Chore: add some helper functions

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
 const fs = require("fs");
 const FireblocksSDK = require("fireblocks-sdk").FireblocksSDK;
+const EthersBridge = require("fireblocks-defi-sdk");
+const Chain = require("fireblocks-defi-sdk");
 
-const create = async () => {
+const chain = Chain[process.env.ETH_CHAIN || "ROPSTEN"]; 
 
-};
-
-const submit = async () => {
-
+const getClient = () => {
     const apiKey = process.env.FIREBLOCKS_API_KEY;
 
     // read private key from file
     const privateKey = await fs.readFile("./privatekey.asc", "utf8");
 
-    const fireblocks = new FireblocksSDK(privateKey, apiKey);
+    const client = new FireblocksSDK(privateKey, apiKey);
+    return client;
+};
+
+const create = async () => {
+    const bridge = new EthersBridge({ 
+        fireblocksApiClient,
+        vaultAccountId: process.env.FIREBLOCKS_SOURCE_VAULT_ACCOUNT || "0",
+        externalWalletId: process.env.FIREBLOCKS_EXTERNAL_WALLET,
+        chain
+    });
+};
+
+const submit = async () => {
+
+    const client = getClient();
 
     const payload = {
         assetId: "ETH",
@@ -29,6 +43,6 @@ const submit = async () => {
         note: "Created by fireblocks SDK"
     };
 
-    const result = await fireblocks.createTransaction(payload);
+    const result = await client.createTransaction(payload);
 };
 
